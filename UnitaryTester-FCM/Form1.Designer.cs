82e8d822603bc592bca36c594d487aa2d6e97281
hye0nchan <81610078+hye0nchan@users.noreply.github.com>
2021-12-29 10:51:21 +0900

    test

---
+++ UnitaryTester-FCM/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace WFormsUserApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.sensor1 = new System.Windows.Forms.Button();
+            this.sensor2 = new System.Windows.Forms.Button();
+            this.sensor3 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.FCM_Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(9, 72);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(296, 366);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(311, 72);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(408, 366);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // sensor1
+            // 
+            this.sensor1.Location = new System.Drawing.Point(12, 22);
+            this.sensor1.Name = "sensor1";
+            this.sensor1.Size = new System.Drawing.Size(75, 23);
+            this.sensor1.TabIndex = 4;
+            this.sensor1.Text = "sensor1";
+            this.sensor1.UseVisualStyleBackColor = true;
+            // 
+            // sensor2
+            // 
+            this.sensor2.Location = new System.Drawing.Point(105, 22);
+            this.sensor2.Name = "sensor2";
+            this.sensor2.Size = new System.Drawing.Size(75, 23);
+            this.sensor2.TabIndex = 5;
+            this.sensor2.Text = "sensor2";
+            this.sensor2.UseVisualStyleBackColor = true;
+            // 
+            // sensor3
+            // 
+            this.sensor3.Location = new System.Drawing.Point(206, 22);
+            this.sensor3.Name = "sensor3";
+            this.sensor3.Size = new System.Drawing.Size(75, 23);
+            this.sensor3.TabIndex = 6;
+            this.sensor3.Text = "sensor3";
+            this.sensor3.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(724, 72);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(296, 366);
+            this.richTextBox3.TabIndex = 8;
+            this.richTextBox3.Text = "";
+            // 
+            // FCM_Timer
+            // 
+            this.FCM_Timer.Tick += new System.EventHandler(this.FCM_Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 460);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.sensor3);
+            this.Controls.Add(this.sensor2);
+            this.Controls.Add(this.sensor1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.RichTextBox richTextBox1;
+        internal System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button sensor1;
+        private System.Windows.Forms.Button sensor2;
+        private System.Windows.Forms.Button sensor3;
+        internal System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Timer FCM_Timer;
+    }
+}
+
---
+++ UnitaryTester/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace WFormsUserApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sensor1 = new System.Windows.Forms.Button();
+            this.sensor2 = new System.Windows.Forms.Button();
+            this.sensor3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sensor1
+            // 
+            this.sensor1.Location = new System.Drawing.Point(16, 32);
+            this.sensor1.Name = "sensor1";
+            this.sensor1.Size = new System.Drawing.Size(75, 23);
+            this.sensor1.TabIndex = 4;
+            this.sensor1.Text = "sensor1";
+            this.sensor1.UseVisualStyleBackColor = true;
+            this.sensor1.Click += new System.EventHandler(this.sensor1_Click);
+            // 
+            // sensor2
+            // 
+            this.sensor2.Location = new System.Drawing.Point(114, 32);
+            this.sensor2.Name = "sensor2";
+            this.sensor2.Size = new System.Drawing.Size(75, 23);
+            this.sensor2.TabIndex = 5;
+            this.sensor2.Text = "sensor2";
+            this.sensor2.UseVisualStyleBackColor = true;
+            this.sensor2.Click += new System.EventHandler(this.sensor2_Click);
+            // 
+            // sensor3
+            // 
+            this.sensor3.Location = new System.Drawing.Point(210, 32);
+            this.sensor3.Name = "sensor3";
+            this.sensor3.Size = new System.Drawing.Size(75, 23);
+            this.sensor3.TabIndex = 6;
+            this.sensor3.Text = "sensor3";
+            this.sensor3.UseVisualStyleBackColor = true;
+            this.sensor3.Click += new System.EventHandler(this.sensor3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sensor1);
+            this.groupBox1.Controls.Add(this.sensor3);
+            this.groupBox1.Controls.Add(this.sensor2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(312, 80);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "sensor BOX";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(18, 115);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(250, 420);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(322, 115);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(250, 420);
+            this.listView2.TabIndex = 9;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView3
+            // 
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(624, 115);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(250, 420);
+            this.listView3.TabIndex = 10;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(347, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 81);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(462, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(100, 81);
+            this.richTextBox2.TabIndex = 12;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(581, 12);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(100, 80);
+            this.richTextBox3.TabIndex = 13;
+            this.richTextBox3.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 547);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button sensor1;
+        private System.Windows.Forms.Button sensor2;
+        private System.Windows.Forms.Button sensor3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
